174e4fa38aa21a99bbf5c533ab5536200711734f
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-05-15 17:33:46 +0200

    Vérification de la chaîne vide

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-const frontElt = document.querySelector('.front');
-const numberFlipper = document.querySelector('.flipper');
+const frontElt = document.querySelector(".front");
+const numberFlipper = document.querySelector(".flipper");
+
+const messageElt = document.querySelector(".message");
+const inputElt = document.querySelector(".guess");
 
 function playErrorAnimation() {
-  frontElt.classList.add('error-animation');
+  frontElt.classList.add("error-animation");
   setTimeout(() => {
-    frontElt.classList.remove('error-animation');
+    frontElt.classList.remove("error-animation");
   }, 400);
 }
 
 function revealSecretNumber() {
-  numberFlipper.classList.add('reveal');
+  numberFlipper.classList.add("reveal");
 }
 
-// mes super animations
+function onCheckBtnClick() {
+  const guessStr = inputElt.value;
+
+  if (guessStr.trim() === "") {
+    messageElt.textContent = "Aucun nombre renseigné";
+    return;
+  }
+}
 
 //playErrorAnimation();
 //revealSecretNumber();
