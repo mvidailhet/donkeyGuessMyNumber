0044b44f78a1ddefd94ceb4d27dbd04a9969ebfe
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-05-15 17:11:02 +0200

    base

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+const frontElt = document.querySelector('.front');
+const numberFlipper = document.querySelector('.flipper');
 
+function playErrorAnimation() {
+  frontElt.classList.add('error-animation');
+  setTimeout(() => {
+    frontElt.classList.remove('error-animation');
+  }, 400);
+}
 
+function revealSecretNumber() {
+  numberFlipper.classList.add('reveal');
+}
+
+function onCheckBtnClick() {
+  playErrorAnimation();
+  //revealSecretNumber();
+}
