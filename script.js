e9a4c4e53df69aa908705a8d72866d40aea73ed2
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-08-17 16:02:17 +0200

    Ajout de l'image et du son de l'âne pour l'animation lorsque le joueur a perdu

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const frontElt = document.querySelector('.front');
 const numberFlipper = document.querySelector('.flipper');
+const donkeyImageElt = document.querySelector('.donkey-image');
+const numberElt = document.querySelector('.number');
+
+const donkeyAudio = new Audio('assets/donkey.mp3');
 
 function playErrorAnimation() {
   frontElt.classList.add('error-animation');
   setTimeout(() => {
     frontElt.classList.remove('error-animation');
   }, 400);
+}
+
+function playLostAnimation() {
+  numberElt.classList.add("hide");
+  donkeyImageElt.classList.add("show");
+  donkeyAudio.play();
 }
 
 function revealSecretNumber() {
@@ -16,3 +26,4 @@ function revealSecretNumber() {
 
 //playErrorAnimation();
 //revealSecretNumber();
+// playLostAnimation();
