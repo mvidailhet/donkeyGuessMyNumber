894f1f1915a2b7d066c607d792e7372038857f64
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-08-17 13:29:20 +0200

    Gestion du click du bouton check Afficher "Il faut préciser un nombre !" si aucun nombre n'est précisé

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const frontElt = document.querySelector('.front');
-const numberFlipper = document.querySelector('.flipper');
+const frontElt = document.querySelector(".front");
+const numberFlipper = document.querySelector(".flipper");
+
+const inputELt = document.querySelector(".guess");
+const messageELt = document.querySelector(".message");
+
+function onCheckButtonClick() {
+  if (inputELt.value === "") {
+    messageELt.textContent = "Il faut préciser un nombre !";
+  }
+}
 
 function playErrorAnimation() {
-  frontElt.classList.add('error-animation');
+  frontElt.classList.add("error-animation");
   setTimeout(() => {
-    frontElt.classList.remove('error-animation');
+    frontElt.classList.remove("error-animation");
   }, 400);
 }
 
 function revealSecretNumber() {
-  numberFlipper.classList.add('reveal');
+  numberFlipper.classList.add("reveal");
 }
 
 // mes super animations
