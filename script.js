945bf910006e425f94d94f7d22c020309a54ed8c
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-08-18 11:47:42 +0200

    Petite fonction pour afficher un emoji pendant un certain temps dans le cercle

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ const numberFlipper = document.querySelector(".flipper");
 const messageElt = document.querySelector(".message");
 const inputElt = document.querySelector(".guess");
 const scoreElt = document.querySelector(".score");
-const highscoreElt = document.querySelector('.highscore');
-const secretNumberElt = document.querySelector('.secret-number');
-const donkeyImageElt = document.querySelector('.donkey-image');
-const numberElt = document.querySelector('.number');
+const highscoreElt = document.querySelector(".highscore");
+const secretNumberElt = document.querySelector(".secret-number");
+const donkeyImageElt = document.querySelector(".donkey-image");
+const numberElt = document.querySelector(".number");
 
-const donkeyAudio = new Audio('assets/donkey.mp3');
+const donkeyAudio = new Audio("assets/donkey.mp3");
+
+let userGuesses = [];
 
 let secretNumber;
+
+let currentMiddleCircleText = "🤫";
 
 resetGame();
 
 // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-function randomIntFromInterval(min, max) { // min and max included 
+function randomIntFromInterval(min, max) {
+  // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
@@ -26,16 +31,20 @@ function playLostAnimation() {
   donkeyAudio.play();
 }
 
+function changeMiddleCircleTextForNMilliseconds(text, milliseconds = 800) {
+  currentMiddleCircleText = numberElt.textContent;
+  numberElt.textContent = text;
+  setTimeout(() => {
+    numberElt.textContent = currentMiddleCircleText;
+  }, milliseconds);
+}
+
 function playErrorAnimation() {
-  numberElt.textContent = '😡';
+  changeMiddleCircleTextForNMilliseconds("😡", 800);
   frontElt.classList.add("error-animation");
   setTimeout(() => {
     frontElt.classList.remove("error-animation");
   }, 400);
-
-  setTimeout(() => {
-    numberElt.textContent = '🤫';
-  }, 800);
 }
 
 function revealSecretNumber() {
@@ -59,19 +68,29 @@ function onCheckBtnClick() {
   const guess = Number(guessStr);
   if (isNaN(guess)) {
     messageElt.textContent = "Ceci n'est pas un nombre";
+    changeMiddleCircleTextForNMilliseconds('🤦‍♂️');
     return;
   }
 
   if (guess < 1 || guess > 20) {
     messageElt.textContent = "Ce nombre n'est pas entre 1 et 20";
+    changeMiddleCircleTextForNMilliseconds('🤦‍♂️');
     return;
   }
+
+  if (userGuesses.includes(guess)) {
+    messageElt.textContent = "Tu as déjà essayé de deviner ce nombre";
+    changeMiddleCircleTextForNMilliseconds('🤷‍♂️');
+    return;
+  }
+
+  userGuesses.push(guess);
 
   let score = Number(scoreElt.textContent);
 
   if (guess === secretNumber) {
     messageElt.textContent = "Tu as gagné !";
-    document.body.classList.add('win-background');
+    document.body.classList.add("win-background");
     revealSecretNumber();
     gameIsFinished = true;
 
@@ -93,7 +112,7 @@ function onCheckBtnClick() {
 
   if (score === 0) {
     messageElt.textContent = "Tu as perdu !";
-    document.body.classList.add('loose-background');
+    document.body.classList.add("loose-background");
     gameIsFinished = true;
     playLostAnimation();
   }
@@ -102,21 +121,20 @@ function onCheckBtnClick() {
 function resetGame() {
   hideSecretNumber();
   resetNumberCircle();
-  numberElt.textContent = '🤫';
+  numberElt.textContent = "🤫";
   messageElt.textContent = "Commence à deviner...";
-  document.body.classList.remove('loose-background');
-  document.body.classList.remove('win-background');
+  document.body.classList.remove("loose-background");
+  document.body.classList.remove("win-background");
   secretNumber = randomIntFromInterval(1, 20);
   gameIsFinished = false;
-  scoreElt.textContent = '20';
-  inputElt.value = '';
-  console.log('secret', secretNumber);
+  scoreElt.textContent = "20";
+  inputElt.value = "";
+  console.log("secret", secretNumber);
   secretNumberElt.textContent = secretNumber;
+  userGuesses = [];
 }
 
 function resetNumberCircle() {
   donkeyImageElt.classList.remove("show");
   numberElt.classList.remove("hide");
 }
-
-
