cf0cb170248d0af362f6d1995dc4d85852d29c71
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-12-06 17:04:07 +0100

    gestion de l'input vide et affichage trop grand / trop petit / gagné avec la comparaison du secret number

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const frontElt = document.querySelector('.front');
-const numberFlipper = document.querySelector('.flipper');
-const donkeyImageElt = document.querySelector('.donkey-image');
-const numberElt = document.querySelector('.number');
+const frontElt = document.querySelector(".front");
+const numberFlipper = document.querySelector(".flipper");
+const donkeyImageElt = document.querySelector(".donkey-image");
+const numberElt = document.querySelector(".number");
 
-const donkeyAudio = new Audio('assets/donkey.mp3');
+const donkeyAudio = new Audio("assets/donkey.mp3");
 
 function playErrorAnimation() {
-  frontElt.classList.add('error-animation');
+  frontElt.classList.add("error-animation");
   setTimeout(() => {
-    frontElt.classList.remove('error-animation');
+    frontElt.classList.remove("error-animation");
   }, 400);
 }
 
@@ -19,7 +19,7 @@ function playLostAnimation() {
 }
 
 function revealSecretNumber() {
-  numberFlipper.classList.add('reveal');
+  numberFlipper.classList.add("reveal");
 }
 
 // mes super animations
@@ -29,6 +29,37 @@ function revealSecretNumber() {
 // playLostAnimation();
 
 const scoreELt = document.querySelector(".score");
-scoreELt.textContent = '30';
+scoreELt.textContent = "30";
 
 numberElt.textContent = "😂";
+
+const inputElement = document.querySelector(".guess");
+
+const messageElt = document.querySelector(".message");
+
+const secretNumber = getRandomNumber(1, 20);
+console.log(secretNumber);
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function onBtnClick() {
+  if (inputElement.value === "") {
+    messageElt.textContent = "Il faut préciser un nombre !";
+  } else {
+    const guess = Number(inputElement.value);
+
+    if (isNaN(guess)) {
+      console.log("This is not a number !");
+    } else {
+      if (guess < secretNumber) {
+        messageElt.textContent = "Trop petit";
+      } else if (guess > secretNumber) {
+        messageElt.textContent = "Trop grand";
+      } else {
+        messageElt.textContent = "gagné";
+      }
+    }
+  }
+}
